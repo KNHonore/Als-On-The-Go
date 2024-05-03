9093fe5d3e349fbb8abba115d8cf85939fdf6a07
KNHonore <108362420+KNHonore@users.noreply.github.com>
2024-05-02 20:59:44 -0400

    adding text and more designing

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 //MENU MODAL//
+const modal = document.querySelector(".menu");
+const openModal = document.querySelector(".menu-btn");
+const closeModal = document.querySelector(".close-btn");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
