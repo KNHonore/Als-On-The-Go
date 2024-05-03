8b39d5be45641e09a4c3ce58c9b888b49b91f4a5
KNHonore <108362420+KNHonore@users.noreply.github.com>
2024-05-02 23:55:43 -0400

    updated modal close

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const modal = document.querySelector(".menu");
 const openModal = document.querySelector(".menu-btn");
 const closeModal = document.querySelector(".close-btn");
+const dialogElement = document.querySelector("dialog");
 
 openModal.addEventListener("click", () => {
   modal.showModal();
@@ -11,3 +12,8 @@ closeModal.addEventListener("click", () => {
   modal.close();
 });
 
+dialogElement.addEventListener("click", (event) => {
+  if (event.target == dialogElement) {
+    modal.close();
+  }
+});
